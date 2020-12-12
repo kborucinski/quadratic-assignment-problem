2f2b8d0b067d7df50c5ed3f0d0a02e58eb607ec8
Krystian Boruciński <krystian.borucinski@gmail.com>
2020-12-12 14:04:22 +0100

    Remove normalize method

--- normalize.js
+++
@@ -1,9 +0,0 @@
-import { sum } from '@sandstreamdev/std/array';
-
-const normalize = scores => {
-  const sumOfScores = sum(scores);
-
-  return scores.map(score => sumOfScores / score);
-};
-
-export default normalize;
